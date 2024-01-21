9ab33546bd496628d585504a6e4291800e4b73fa
shallondev <shallon.frances.pawlickpotts@gmail.com>
2024-01-21 13:26:59 -0500

    finished

--- commerce/auctions/urls.py
+++ commerce/auctions/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create_listing/", views.create_listing, name="create_listing"),
+    path('listing_page/<str:listing_title>/', views.listing_page, name='listing_page'),
+    path('watchlist/', views.watchlist, name="watchlist"),
+    path('categories/', views.categories, name="categories"),
 ]
